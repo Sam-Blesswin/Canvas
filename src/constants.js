7ac22cdc909d9d4b2f47484437de44744ad31f0b
Sam <samblesswinedu@gmail.com>
2024-01-11 00:47:43 -0700

    Added toolbox with styling

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const COLORS = {
+  BLACK: "#000000",
+  RED: "#FF0000",
+  GREEN: "#00FF00",
+  BLUE: "#0000FF",
+  YELLOW: "#FFFF00",
+  ORANGE: "#FFA500",
+  PURPLE: "#800080",
+};
+
+export default COLORS;
